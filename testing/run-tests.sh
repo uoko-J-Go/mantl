b1b381545195e38cb2efa6127401375cd60703f6
siddharthist <langston.barrett@gmail.com>
2016-03-15 02:22:41 -0700

    Quoting to prevent globbing and word splitting

--- testing/run-tests.sh
+++ testing/run-tests.sh
@@ -5,7 +5,7 @@ set -ev
 ## This script is control when testing should be triggered on CI systems
 
 ### filter out docfiles
-commit_range_minus_docfiles=$(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -v -e '^docs/' -e 'md$' -e 'rst$')
+commit_range_minus_docfiles=$(git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep -v -e '^docs/' -e 'md$' -e 'rst$')
 if [ -z "$commit_range_minus_docfiles" ]; then
 	echo "Skipping build that only has doc changes"
 	exit 0
@@ -26,14 +26,14 @@ fi
 
 ### if the build wasn't skipped, let's set up
 python2 testing/test-health-checks.py
-ssh-keygen -N '' -f ~/.ssh/id_rsa && eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa
-ln -sf $ANSIBLE_PLAYBOOK terraform.yml
-ln -sf $TERRAFORM_FILE terraform.tf
-mkdir $HOME/bin && export PATH=$PATH:$HOME/bin
+ssh-keygen -N '' -f ~/.ssh/id_rsa && eval "$(ssh-agent)" && ssh-add ~/.ssh/id_rsa
+ln -sf "$ANSIBLE_PLAYBOOK" terraform.yml
+ln -sf "$TERRAFORM_FILE" terraform.tf
+mkdir "$HOME/bin" && export PATH=$PATH:$HOME/bin
 export TERRAFORM_VERSION=0.6.8
 #### GCE doesn't like . in resource.name, so replace "." with "-"
 curl -SL -o terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-unzip -d $HOME/bin terraform.zip
+unzip -d "$HOME/bin" terraform.zip
 
 ### if nothing happened during the setup, let's build
 python2 testing/build-cluster.py
