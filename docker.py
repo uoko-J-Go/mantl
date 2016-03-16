7b3b07e4f6d5deb9e65744b84c25f507d6d77f1b
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-03-16 13:49:10 -0500

    Rename docker_setup -> docker, add cli-type args
    We are rolling setup and deploy commands into one python script. It
    doesn't have much functionality, but this PR is getting out of scope
    quickly, and it's needed for CI.

--- docker.py
+++ docker.py
@@ -3,97 +3,122 @@ from __future__ import print_function
 
 import os
 import os.path
+import logging
 
-from os import symlink
 from os.path import exists, join
 from shlex import split
 from sys import argv, exit
 from subprocess import call
 
-def link_or_generate_ssh_keys():
-    if 'SSH_KEY' not in os.environ:
-        os.environ['SSH_KEY'] = 'id_rsa'
 
-    ssh_key = join(os.environ['MANTL_CONFIG_DIR'], os.environ['SSH_KEY'])
+def symlink_force(source, link_name):
+    """Equivalent to adding -f flag to bash invocation of ln"""
+    if exists(link_name):
+        os.remove(link_name)
+
+    logging.info("Symlinking {} to {}".format(source, link_name))
+    os.symlink(source, link_name)
+
+
+def link_or_generate_ssh_keys():
+    """Ensures that valid ssh keys are symlinked to /root/.ssh"""
+    if 'MANTL_SSH_KEY' not in os.environ:
+        os.environ['MANTL_SSH_KEY'] = 'id_rsa'
+
+    ssh_key = join(os.environ['MANTL_CONFIG_DIR'], os.environ['MANTL_SSH_KEY'])
     if not exists(ssh_key):
         call(split('ssh-keygen -N "" -f {}'.format(ssh_key)))
 
-    print("Symlinking {} to /root/.ssh/id_rsa".format(ssh_key))
-    symlink(ssh_key, '/root/.ssh/id_rsa')
-
+    symlink_force(ssh_key, '/root/.ssh/id_rsa')
     ssh_key += '.pub'
-    print("Symlinking {} to /root/.ssh/id_rsa.pub".format(ssh_key))
-    symlink(ssh_key, '/root/.ssh/id_rsa.pub')
+    symlink_force(ssh_key, '/root/.ssh/id_rsa.pub')
 
 
 def link_terraform_files():
+    """Ensures that provided/chosen terraform files are symlinked"""
     cfg_d = os.environ['MANTL_CONFIG_DIR']
     tfs = [join(cfg_d, f) for f in os.listdir(cfg_d) if f.endswith('.tf')]
     if len(tfs):
         for tf in tfs:
             base = os.path.basename(tf)
-            print("Symlinking {} to {}".format(tf, base))
-            symlink(tf, base)
+            symlink_force(tf, base)
     else:
         if 'MANTL_PROVIDER' not in os.environ:
-            print("mantl.readthedocs.org for provider")
+            logging.critical("mantl.readthedocs.org for provider")
             exit(1)
         tf = 'terraform/{}.sample.tf'.format(os.environ['MANTL_PROVIDER'])
 
-        print("Symlinking {} to mantl.tf".format(tf))
-        symlink(tf, 'mantl.tf')
+        symlink_force(tf, 'mantl.tf')
 
 
 def link_ansible_playbook():
+    """Ensures that provided/sample ansible playbook is symlinked"""
     ansible_playbook = join(os.environ['MANTL_CONFIG_DIR'], 'mantl.yml')
     if not exists(ansible_playbook):
         ansible_playbook = 'sample.yml'
 
-    print("Symlinking {} to mantl.yml".format(ansible_playbook))
-    symlink(ansible_playbook, 'mantl.yml')
+    symlink_force(ansible_playbook, 'mantl.yml')
 
 
 def link_or_generate_security_file():
+    """Ensures that security file exists and is symlinked"""
     security_file = join(os.environ['MANTL_CONFIG_DIR'], 'security.yml')
-    if exists(security_file):
-        print("Symlinking {} to security.yml".format(security_file))
-        symlink(security_file, 'security.yml')
-    else:
-        print("Generating security.yml via security-setup")
+    if not exists(security_file):
+        logging.info("Generating {} via security-setup".format(security_file))
         call(split('./security-setup --enable=false'))
+        os.rename('security.yml', security_file)
+
+    symlink_force(security_file, 'security.yml')
 
 
 def setup():
+    """Run all setup commands, saving files to MANTL_CONFIG_DIR"""
     link_or_generate_ssh_keys()
     link_ansible_playbook()
     link_terraform_files()
     link_or_generate_security_file()
 
 
-def deploy():
+def terraform():
+    """Run terraform commands. Assumes that setup has been run"""
     link_or_generate_ssh_keys()
-    # ssh-add
-    # terraform get
-    # terraform apply -state=$TERRAFORM_STATE
-    # ansible-playbook /mantl/playbooks/wait-for-hosts.yml
-    # ansible-playbook mantl.yml -e @security.yml
+    call(split("ssh-add"))
+    call(split("terraform get"))
+    call(split("terraform apply -state=$TERRAFORM_STATE"))
 
-def test():
-    # testing run_test|build-cluster.py
+def ansible():
+    """Run ansible playbooks. Assumes that setup and terraform have been run"""
+    link_or_generate_ssh_keys()
+    call(split("ssh-add"))
+    call(split("ansible-playbook playbooks/upgrade-packages.yml -e @security.yml"))
+    call(split("ansible-playbook mantl.yml -e @security.yml"))
 
 
 if __name__ == "__main__":
 
+    logfmt = "%(levelname)s\t%(asctime)s\t%(message)s"
+    logging.basicConfig(format=logfmt, level=logging.INFO)
+
     if 'MANTL_CONFIG_DIR' not in os.environ:
-        print('mantl.readthedocs.org for mantl config dir')
+        logging.critical('mantl.readthedocs.org for mantl config dir')
         exit(1)
 
-    if argv:
+    #TODO: replace this with either click or pypsi
+    if len(argv) > 1:
         if argv[1] == 'setup':
             setup()
+        elif argv[1] == 'terraform':
+            terraform()
+        elif argv[1] == 'ansible':
+            ansible()
         elif argv[1] == 'deploy':
-            deploy()
-        elif argv[1] == 'test':
-            test()
+            setup()
+            terraform()
+            ansible()
+        else:
+            logging.critical("Usage: docker.py [CMD]")
+            exit(1)
+            
     else:
-        print("Usage: docker.py [CMD]")
+        logging.critical("Usage: docker.py [CMD]")
+        exit(1)
