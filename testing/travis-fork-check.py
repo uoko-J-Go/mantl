cce31200f6aeafec505d15cc5a8c6b79533c9333
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-05-04 10:18:36 -0500

    Log message instead of output for decryption
    When we run the test, the only thing we need to see in the build log is
    the fact that we are decrypting the file, and moving on.

--- testing/travis-fork-check.py
+++ testing/travis-fork-check.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import logging
 import os
-import subprocess
 import shlex
+import subprocess
+import sys
 
 def check_travis_repo_slug():
     if not os.environ['TRAVIS_REPO_SLUG'].startswith('CiscoCloud/'):
         logging.warning("Currently, we can't unlock deploy keys for forks of Mantl, so we are skipping the build")
-        os.exit(0)
+        sys.exit(0)
     else:
+        logging.info("Decrypting OS ssh key")
         cmd = "openssl aes-256-cbc -K {key} -iv {iv} -in ci.enc -out testing/ci -d".format(
                 key=os.environ['encrypted_6a9d32f3e0bd_key'],
                 iv=os.environ['encrypted_6a9d32f3e0bd_iv'])
-        out = subprocess.check_output(shelx.split(cmd))
-        print(out)
+        sys.exit(subprocess.call(shelx.split(cmd)))
