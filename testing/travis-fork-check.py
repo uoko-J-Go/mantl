ca6ccd66b8ebaed86260601b38f698de0db6aee7
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-05-04 13:26:50 -0500

    Move message & exit code for forks to build step
    I was getting ready for this to be tested on a fork, when I realized
    that my message and exit code in the fork check script would not
    actually skip the build, since the message and exit code were not in the
    build step. So, I moved my message back to its original place, and the
    fork check now just prevents decryption from happening for forks

--- testing/travis-fork-check.py
+++ testing/travis-fork-check.py
@@ -7,10 +7,7 @@ import subprocess
 import sys
 
 def check_travis_repo_slug():
-    if not os.environ['TRAVIS_REPO_SLUG'].startswith('CiscoCloud/'):
-        logging.warning("Currently, we can't unlock deploy keys for forks of Mantl, so we are skipping the build")
-        sys.exit(0)
-    else:
+    if os.environ['TRAVIS_REPO_SLUG'].startswith('CiscoCloud/'):
         logging.info("Decrypting OS ssh key")
         cmd = "openssl aes-256-cbc -K {key} -iv {iv} -in ci.enc -out testing/ci -d".format(
                 key=os.environ['encrypted_6a9d32f3e0bd_key'],
