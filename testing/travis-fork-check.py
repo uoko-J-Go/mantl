89e810187fcb372eac4e474111ee0ee6a4d5eb33
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-05-04 15:18:11 -0500

    TRAVIS Check env for encryption vars

--- testing/travis-fork-check.py
+++ testing/travis-fork-check.py
@@ -4,18 +4,23 @@ import logging
 import os
 import shlex
 import subprocess
+import sys
 
 def check_travis_repo_slug():
-    logging.info("Decrypting OS ssh key")
-    cmd = "openssl aes-256-cbc -K {key} -iv {iv} -in ci.enc -out testing/ci -d".format(
-            key=os.environ['encrypted_6a9d32f3e0bd_key'],
-            iv=os.environ['encrypted_6a9d32f3e0bd_iv'])
-    returncode = subprocess.call(shlex.split(cmd))
-    if returncode != 0:
-        logging.critical("Description failed. This is likely due to commit coming from fork")
+    if 'encrypted_6a9d32f3e0bd_key' not in os.environ:
+        logging.warning("Decryption disabled. Setting MANTL_CI_FORK_CHECK to 1")
         os.environ['MANTL_CI_FORK_CHECK'] = "1"
     else:
-        logging.info("Decryption successful")
+        logging.info("Decrypting OS ssh key")
+        cmd = "openssl aes-256-cbc -K {key} -iv {iv} -in ci.enc -out testing/ci -d".format(
+                key=os.environ['encrypted_6a9d32f3e0bd_key'],
+                iv=os.environ['encrypted_6a9d32f3e0bd_iv'])
+        returncode = subprocess.call(shlex.split(cmd))
+        if returncode != 0:
+            logging.critical("Description failed!")
+            sys.exit(1)
+        else:
+            logging.info("Decryption successful")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
