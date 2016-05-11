5ab511e3749558d31d04e4ca170c42eb92dab6e7
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-05-11 15:12:55 -0500

    TRAVIS Start decoupling decryption and fork check
    Detecting forks and decrypting the SSH key are tightly coupled. This is
    the first step in eliminating that. A change that happened outside of
    the code here is that I added ARE_SECRETS_AVAILABLE to travis's secrets
    list.

--- testing/travis-fork-check.py
+++ testing/travis-fork-check.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import logging
 import os
 import subprocess
 import sys
 
 def check_travis_repo_slug():
     if 'encrypted_6a9d32f3e0bd_key' not in os.environ:
-        logging.warning("Decryption disabled. Setting MANTL_CI_FORK_CHECK to 1")
+        sys.stderr.write("Decryption disabled. Setting MANTL_CI_FORK_CHECK to 1")
         print("1")
     else:
-        logging.info("Decrypting OS ssh key")
+        sys.stderr.write("Decrypting OS ssh key")
         cmd = ["openssl",
                 "aes-256-cbc",
                 "-K", os.environ['encrypted_6a9d32f3e0bd_key'],
@@ -20,11 +19,9 @@ def check_travis_repo_slug():
                 ]
         returncode = subprocess.call(cmd)
         if returncode != 0:
-            logging.critical("Decryption failed!")
-            sys.exit(1)
+            sys.exit("Decryption failed!")
         else:
-            logging.info("Decryption successful")
+            sys.stderr.write("Decryption successful")
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     check_travis_repo_slug()
